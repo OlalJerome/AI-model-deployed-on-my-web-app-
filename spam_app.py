7e2f5fd364b1a7fca87d230a5206dad487dbbb2e
OLAL AMOS JEROME <89550126+OlalJerome@users.noreply.github.com>
2025-04-14 00:28:15 +0300

    Update spam_app.py

--- spam_app.py
+++ spam_app.py
@@ -1,21 +1,26 @@
-import joblib
-import gdown
 import streamlit as st
+import joblib
+import os
+import gdown
 
-# Google Drive file IDs (not full URL)
+# Google Drive file IDs
 model_id = "1AS9xpZMl4zwW32IJoV92gresTQkceb1A"
 vectorizer_id = "1KCWuep5WurRz5KiSSjKg8Sf7IOrm64ov"
 
-# Download if not already present
-if not os.path.exists("spam_classifier_nb.pkl"):
-    gdown.download(f"https://drive.google.com/uc?id={model_id}", "spam_classifier_nb.pkl", quiet=False)
+# File names
+model_file = "spam_classifier_nb.pkl"
+vectorizer_file = "vectorizer.pkl"
 
-if not os.path.exists("vectorizer.pkl"):
-    gdown.download(f"https://drive.google.com/uc?id={vectorizer_id}", "vectorizer.pkl", quiet=False)
+# Download if not present
+if not os.path.exists(model_file):
+    gdown.download(f"https://drive.google.com/uc?id={model_id}", model_file, quiet=False)
+
+if not os.path.exists(vectorizer_file):
+    gdown.download(f"https://drive.google.com/uc?id={vectorizer_id}", vectorizer_file, quiet=False)
 
 # Load model and vectorizer
-model = joblib.load("spam_classifier_nb.pkl")
-vectorizer = joblib.load("vectorizer.pkl")
+model = joblib.load(model_file)
+vectorizer = joblib.load(vectorizer_file)
 
 # Streamlit app UI
 st.title("📬 Email Spam Detector")
